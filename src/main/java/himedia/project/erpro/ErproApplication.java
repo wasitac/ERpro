2dde368dfcb09fb9f90d42bac28a3059a5676d02
wasitac <67859497+wasitac@users.noreply.github.com>
2024-01-22 21:04:43 +0900

    Init: erpro 프로젝트 생성

---
+++ src/main/java/himedia/project/erpro/ErproApplication.java
@@ -0,0 +1,13 @@
+package himedia.project.erpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/himedia/project/erpro/ErproApplicationTests.java
@@ -0,0 +1,13 @@
+package himedia.project.erpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
