adcf90b3c1fe0e7344d12bcbd5175792f28f5265
hola <kimjw0913@gmail.com>
2024-01-23 18:30:15 +0900

    Add: 로고 추가, 파비콘 추가, App.js Route FindPassword 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage  from './components/views/pages/LoginPage/LoginPage';
+import FindPasswordPage from './components/views/pages/FindPasswordPage/FindPasswordPage';
 import MainPage from './components/views/pages/MainPage/MainPage';
 import ProfilePage from './components/views/pages/ProfilePage/ProfilePage'
 
@@ -8,11 +9,12 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-          <Routes>
-          	<Route path="login" element={<LoginPage/>}/>
-            <Route path="" element={<MainPage/>}/>
-            <Route path="profile" element={<ProfilePage/>}/>
-        </Routes>
+      	<Routes>
+	      	<Route path="login" element={<LoginPage/>}/>
+          <Route path="findpassword" element={<FindPasswordPage/>}/>
+	        <Route path="" element={<MainPage/>}/>
+	        <Route path="profile" element={<ProfilePage/>}/>
+      	</Routes>
       </BrowserRouter>
     </div>
   );
