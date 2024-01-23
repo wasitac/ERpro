117be9bb3f34b2aba8c88e5a28320e6108b6811f
Joowon913 <kimjw0913@gmail.com>
2024-01-23 23:16:11 +0900

    Add: LoginPage 디자인 수정

--- src/main/frontend/src/components/views/pages/LoginPage/LoginPage.js
+++ src/main/frontend/src/components/views/pages/LoginPage/LoginPage.js
@@ -1,92 +1,92 @@
 /** 
  * 김주원
- */ 
+ * ../../../../img/logo2.png
+ */
 import React from 'react';
-import { Button, Form, Input } from 'antd';
+import {useNavigate} from "react-router-dom";
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Row, Col, Image } from 'antd';
+
 import logo2 from '../../../../img/logo2.png';
 
-const onFinish = (values) => {
-  console.log('Success:', values);
-};
-const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
-
-const LoginPage = () => (
-  <Form
-    name="basic"
-    labelCol={{
-      span: 8,
-    }}
-    wrapperCol={{
-      span: 16,
-    }}
-    style={{
-      maxWidth: 600,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <div>
-      <img src={logo2} alt="로고" style={{width:"50%"}}/>
-    </div>
-      <Form.Item
-        name="userNo"
-        rules={[
-          {
-            required: true,
-            message: '사원번호를 입력해주세요.',
-          },
-        ]}
-      >
-        <Input placeholder='사원번호를 입력하세요.'/>
-      </Form.Item>
-
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: '비밀번호를 입력해주세요.',
-          },
-        ]}
-      >
-        <Input.Password placeholder='비밀번호를 입력하세요.' />
-      </Form.Item>
-
-  {/*     
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-  */}
+function LoginPage() {
+  const navigate = useNavigate();
   
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" shape="round" size="large" htmlType="submit" style={{backgroundColor:"#66bd00"}}>
-          로그인
-        </Button>
-        <div>
-          <a href="/findpassword" style={{color:"#66bd00"}}>
-            비밀번호를 잊어버리셨나요?
-          </a>
+  // form submit
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+    // TODO:: login api 호출
+    
+    // TODO:: login api 결과에 따른 페이지 이동 제어, 무조건 메인으로 이동하도록 임시 하드코딩
+    navigate('/');
+  };
+  
+  return (
+    <Row justify="center" align="middle" style={{ height: '100vh' }}>
+      <Col span={6}>
+        <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+          <Image src={logo2} alt="Company Logo" preview={false} style={{ width: '60%'}}/>
         </div>
-      </Form.Item>
-  </Form>
-);
-
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          size="large"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="userid"
+            rules={[
+              {
+                required: true,
+                message: '사원번호를 입력해주세요!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="사원번호를 입력하세요" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: '비밀번호를 입력해주세요!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+            />
+          </Form.Item>
+          <Form.Item
+            style={{ textAlign: 'center' }}
+          >
+            <Button
+              type="primary"
+              shape="round"
+              size="large"
+              htmlType="submit"
+              className="login-form-button"
+              style={{ backgroundColor: '#66bd00'}}
+            >
+              Log in
+            </Button>
+            <div
+              style={{ marginTop: '10px' }}
+            >
+              <a style={{color: '#66bd00'}} onClick={(event) => {
+                event.preventDefault();
+                navigate('/findpassword');
+              }}>비밀번호를 잊어버렸어요</a>
+            </div>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
 export default LoginPage;
