3c5b853ede411e4af3ca5b7ba50f891b035ae41c
bnanadog <vebmisesk@gmail.com>
2024-01-23 21:53:57 +0900

    Feat: 헤더, 네브바 작업

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './components/views/pages/MainPage/MainPage';
-import ProfilePage from './components/views/pages/ProfilePage/ProfilePage'
+import ProfilePage from './components/views/pages/ProfilePage/ProfilePage';
+import Navbar from './components/views/pages/Header/Navbar';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-          <Routes>
-            <Route path="" element={<MainPage/>}/>
-            <Route path="profile" element={<ProfilePage/>}/>
+        <Routes>
+          <Route path="" element={<MainPage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="header" element={<Navbar />} />
         </Routes>
       </BrowserRouter>
     </div>
