cbdb737691b0fc14a0fcef206f95b789be3ace9e
wasitac <67859497+wasitac@users.noreply.github.com>
2024-01-25 11:56:51 +0900

    Add: @Entity 어노테이션 추가

--- src/main/java/himedia/project/erpro/entity/Account.java
+++ src/main/java/himedia/project/erpro/entity/Account.java
@@ -1,11 +1,18 @@
 package himedia.project.erpro.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	long id;			// 거래처 코드
 	long user_id;		// 담당자 id			user에서 id를 외래키로 참조
 	String b_no;		// 사업자 등록 번호
