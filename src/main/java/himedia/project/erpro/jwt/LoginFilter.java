6dd1967743118a91c4a558232fd1c2c5e69f0ba4
joowon913 <kimjw0913@gmail.com>
2024-01-28 17:52:10 +0900

    Fix: JWT 토큰 만료시간 변경

--- src/main/java/himedia/project/erpro/jwt/LoginFilter.java
+++ src/main/java/himedia/project/erpro/jwt/LoginFilter.java
@@ -47,7 +47,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         String role = auth.getAuthority();
 
-        String token = jwtUtil.createJwt(customUserDetails, role, 60*60*10L);
+        String token = jwtUtil.createJwt(customUserDetails, role, 10*60*60*1000L);
 
         response.addHeader("Authorization","Bearer " + token );
     }
