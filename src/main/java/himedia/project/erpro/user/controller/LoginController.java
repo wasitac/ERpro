f4a22e043b4f374b6d97465ac7cd5bbf70366dbe
joowon913 <kimjw0913@gmail.com>
2024-01-28 15:10:07 +0900

    Add: controller에서 로그인 계정정보 확인 예제코드 추가

--- src/main/java/himedia/project/erpro/user/controller/LoginController.java
+++ src/main/java/himedia/project/erpro/user/controller/LoginController.java
@@ -1,12 +1,32 @@
 package himedia.project.erpro.user.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RestController
 public class LoginController {
 	@GetMapping("/login")
 	public String login() {
+		
+		// 로그인 계정정보, role 확인 예제코드
+		String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+		GrantedAuthority auth = iterator.next();
+
+		String role = auth.getAuthority();
+
+		System.out.println("userId: "+userId);
+		System.out.println("role: "+role);
+
 		return "로그인 페이지";
 	}
 }
