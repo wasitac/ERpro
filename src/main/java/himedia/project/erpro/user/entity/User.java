bd91030459c723c4b898865165a09faa3868a030
joowon913 <kimjw0913@gmail.com>
2024-01-29 00:20:57 +0900

    Add: User entity setter 추가 및 private 제거

--- src/main/java/himedia/project/erpro/user/entity/User.java
+++ src/main/java/himedia/project/erpro/user/entity/User.java
@@ -12,19 +12,16 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 
 @Entity
 @Setter
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
