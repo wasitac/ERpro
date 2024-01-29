ae8737b224e2b31763bb3d663f6f13d209c026cc
joowon913 <kimjw0913@gmail.com>
2024-01-29 23:56:35 +0900

    Add: 거래처 등록 폼 이메일 형식체크 추가

--- src/main/frontend/src/components/views/commons/Modal/Form/AccountForm.js
+++ src/main/frontend/src/components/views/commons/Modal/Form/AccountForm.js
@@ -143,6 +143,10 @@ const AccountForm = () => {
                 required: true,
                 message: "이메일을 입력해주세요",
               },
+              {
+                type: "email",
+                message: "올바른 이메일 형식이 아닙니다.",
+              },
             ]}
           >
             <Input />
