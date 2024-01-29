1ec7f3a304ed4fef20b36e0e4e66b9b47b5f3d1d
bnanadog <vebmisesk@gmail.com>
2024-01-29 09:19:54 +0900

    Docs: 줄 수정

--- src/main/frontend/src/components/views/commons/Modal/AddModal.js
+++ src/main/frontend/src/components/views/commons/Modal/AddModal.js
@@ -1,21 +1,8 @@
-<<<<<<< HEAD:src/main/frontend/src/components/views/pages/Modal/AddModal.js
-import React, { useState } from 'react';
-import { Button, Modal } from 'antd';
-import "../../Header/reset.css";
-import OrderAdd from '../OrderPage/OrderAdd';
-import OrderAddDataTable from '../OrderPage/OrderAddDataTable';
-import SalesAddDataTable from '../OrderPage/SalesAddDataTable';
-import SalesAdd from '../OrderPage/SalesAdd';
-import EstimateAdd from '../EstimatePage/EstimateAdd';
-import EstimateAddDataTable from '../EstimatePage/EstimateAddDataTable';
-
-=======
 import React, { useState } from "react";
 import { Button, Modal } from "antd";
 import "../../Header/reset.css";
 import OrderAdd from "../../pages/OrderPage/OrderAdd";
 import OrderAddDataTable from "../../pages/OrderPage/OrderAddDataTable";
->>>>>>> bd91030459c723c4b898865165a09faa3868a030:src/main/frontend/src/components/views/commons/Modal/AddModal.js
 
 const AddModal = () => {
   const [loading, setLoading] = useState(false);
@@ -54,27 +41,18 @@ const AddModal = () => {
           <Button key="back" onClick={handleCancel}>
             취소
           </Button>,
-<<<<<<< HEAD:src/main/frontend/src/components/views/pages/Modal/AddModal.js
-          <Button key="submit" type="primary" onClick={handleOk} style={{ backgroundColor: '#66dd00' }}>
-=======
           <Button
             key="submit"
             type="primary"
             onClick={handleOk}
             style={{ backgroundColor: "#66dd00" }}
           >
->>>>>>> bd91030459c723c4b898865165a09faa3868a030:src/main/frontend/src/components/views/commons/Modal/AddModal.js
             저장
           </Button>,
         ]}
       >
-<<<<<<< HEAD:src/main/frontend/src/components/views/pages/Modal/AddModal.js
-        <EstimateAdd />
-        <EstimateAddDataTable />
-=======
         <OrderAdd />
         <OrderAddDataTable />
->>>>>>> bd91030459c723c4b898865165a09faa3868a030:src/main/frontend/src/components/views/commons/Modal/AddModal.js
       </Modal>
     </>
   );
