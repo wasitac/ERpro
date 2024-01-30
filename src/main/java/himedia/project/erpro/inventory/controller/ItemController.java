4b7cf22f330a4f1ef81c0975ae1a9223b2463964
joowon913 <kimjw0913@gmail.com>
2024-01-30 23:39:23 +0900

    Add: Item entity setter 추가 및 물품 상세데이터 조회 API 작업

--- src/main/java/himedia/project/erpro/inventory/controller/ItemController.java
+++ src/main/java/himedia/project/erpro/inventory/controller/ItemController.java
@@ -5,13 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import himedia.project.erpro.common.Message;
 import himedia.project.erpro.inventory.entity.Item;
@@ -30,6 +24,14 @@ public class ItemController {
 	public ResponseEntity<Message> item() {
 		List<Item> dataList = itemService.getItemAll();
 		Message returnData = new Message("", dataList);
+		return new ResponseEntity<>(returnData, HttpStatus.OK);
+	}
+
+	// 물품 상세데이터 조회 - 김주원
+	@GetMapping("/item/{id}")
+	public ResponseEntity<Message> detailItem(@PathVariable(value="id") Long id) {
+		Optional<Item> data = itemService.getItemById(id);
+		Message returnData = new Message("", data);
 		return new ResponseEntity<>(returnData, HttpStatus.OK);
 	}
 	
--- src/main/java/himedia/project/erpro/inventory/entity/Item.java
+++ src/main/java/himedia/project/erpro/inventory/entity/Item.java
@@ -5,14 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
