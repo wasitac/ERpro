8de3defb4bc4b2ee031908125aa2e594397b7698
joowon913 <kimjw0913@gmail.com>
2024-02-01 18:21:58 +0900

    Feat: 임시비밀번호 메일 발송 기능 추가

--- src/main/java/himedia/project/erpro/member/controller/LoginController.java
+++ src/main/java/himedia/project/erpro/member/controller/LoginController.java
@@ -1,16 +1,27 @@
 package himedia.project.erpro.member.controller;
 
+import himedia.project.erpro.common.Message;
+import himedia.project.erpro.member.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.Iterator;
 
 @RestController
+@RequiredArgsConstructor
 public class LoginController {
+
+	private final LoginService loginService;
+
 	@GetMapping("/login")
 	public String login() {
 		
@@ -28,5 +39,13 @@ public class LoginController {
 		System.out.println("role: "+role);
 
 		return "로그인 페이지";
+	}
+
+	// 비밀번호 찾기 - 김주원
+	@PostMapping("/findpassword")
+	public ResponseEntity<Message> findPassword(@RequestBody Long memberId) {
+
+		String result = loginService.findPassword(memberId);
+		return new ResponseEntity<>(new Message(result), HttpStatus.OK);
 	}
 }
