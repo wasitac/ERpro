523a5d97bf3c9c3f7fd9b5983ce6d99e8470fde6
joowon913 <kimjw0913@gmail.com>
2024-02-02 20:32:23 +0900

    Add: BomForm 수정

--- src/main/frontend/src/components/views/commons/Modal/Form/BomForm.js
+++ src/main/frontend/src/components/views/commons/Modal/Form/BomForm.js
@@ -1,97 +1,167 @@
-import { Button, Card, Form, Input, Select, Space, Typography } from "antd";
-import { CloseOutlined } from '@ant-design/icons';
+import { useState, useEffect } from "react";
+import fetchApi from "../../../../../modules/api";
+import { Button, Form, Input, Select, Space } from "antd";
+import {MinusCircleOutlined, PlusOutlined} from "@ant-design/icons";
 
 const BomForm = ({ form }) => {
-    // 금액 계산 로직
-    const calculateAmount = (buyPrice, requiredAmount) => {
-        const price = Number(buyPrice) * Number(requiredAmount);
-        return isNaN(price) ? undefined : price;
+    // 물품 목록
+    const [itemList, setItemList] = useState([]);
+
+    useEffect(() => {
+        // 물품 목록조회
+        fetchItemList();
+    }, []);
+
+    // 물품 목록 조회
+    const fetchItemList = async () => {
+        try {
+            const response = await fetchApi.get('/item');
+            setItemList(response.data.data);
+        } catch (error) {
+            console.error("물품 목록 조회 에러:", error);
+        }
+    };
+
+    // 자재 선택 시 매입단가 설정
+    const onMaterialSelect = (value, formInstance, name) => {
+        const selectedMaterial = itemList.find(item => item.id === value);
+        if (selectedMaterial) {
+            const subItemList = formInstance.getFieldValue('subItemList');
+            const updatedSubItemList = [...subItemList];
+            updatedSubItemList[name].buyPrice = selectedMaterial.buyPrice;
+
+            // 금액 변경
+            if(!isNaN(updatedSubItemList[name].buyPrice * updatedSubItemList[name].requiredAmount)) {
+                updatedSubItemList[name].price = updatedSubItemList[name].buyPrice * updatedSubItemList[name].requiredAmount;
+            }
+
+
+            formInstance.setFieldsValue({
+                subItemList: updatedSubItemList,
+            });
+        }
+    };
+
+    // 소요량 입력에 따른 금액 계산
+    const onPriceChange  = (value, formInstance, name) => {
+        const subItemList = formInstance.getFieldValue('subItemList');
+        const updatedSubItemList = [...subItemList];
+
+        if(!isNaN(updatedSubItemList[name].buyPrice * updatedSubItemList[name].requiredAmount)) {
+            updatedSubItemList[name].price = updatedSubItemList[name].buyPrice * updatedSubItemList[name].requiredAmount;
+        }
+
+        formInstance.setFieldsValue({
+            subItemList: updatedSubItemList,
+        });
     };
 
     return (
         <>
-            <Form.Item label="물품" name="itemId">
-                <Input />
+            <Form.Item name="id" noStyle>
+                <Input type="hidden" />
             </Form.Item>
-
-            {/* Nest Form.List */}
-            <Form.Item label="자제목록">
-                <Form.List name='list'>
-                    {(subFields, subOpt) => (
+            <Form.Item
+                label="제품"
+                name="itemId"
+                rules={[{ required: true, message: '제품을 선택해주세요.' }]}
+            >
+                <Select>
+                    {itemList.map((item) => (
+                        item.sort === "제품" && (
+                            <Select.Option key={item.id} value={item.id}>
+                                {item.itemName}
+                            </Select.Option>
+                        )
+                    ))}
+                </Select>
+            </Form.Item>
+            <Form.Item
+                label="자재"
+            >
+                <Form.List
+                    name="bomList"
+                    // initialValue={[
+                    //   {
+                    //     itemId: 1,
+                    //     price: 1000,
+                    //     note: "",
+                    //   },
+                    //   {
+                    //     itemId: 3,
+                    //     price: 3000,
+                    //     note: "",
+                    //   },
+                    // ]}
+                >
+                    {(fields, { add, remove }) => (
                         <div
                             style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                rowGap: 16,
+                                display: "flex",
+                                justifyContent: "center",
+                                flexDirection: "column",
+                                rowGap: 15,
                             }}
                         >
-                            {subFields.map((subField,index) => (
-                                <Space key={subField.key}>
-                                    <Form.Item noStyle name={[subField.name, 'materialId']}>
-                                        <Select style={{ minWidth: 120 }}>
-                                            <Select.Option value="SALES">영업</Select.Option>
-                                            <Select.Option value="PRODUCTION">생산</Select.Option>
-                                            <Select.Option value="HR">인사</Select.Option>
-                                            <Select.Option value="LOGISTICS">물류</Select.Option>
-                                            <Select.Option value="ADMIN">관리</Select.Option>
+                            {fields.map(({ key, name, ...restField }) => (
+                                <Space key={key} align={"baseline"}>
+                                    <Form.Item
+                                        noStyle
+                                        {...restField}
+                                        name={[name, 'materialId']}
+                                        rules={[{ required: true, message: '자재를 선택해주세요.' }]}
+                                    >
+                                        <Select style={{ minWidth: 120 }} onChange={(value) => onMaterialSelect(value, form, name)}>
+                                            {itemList.map((item) => (
+                                                <Select.Option key={item.id} value={item.id}>
+                                                    {item.itemName}
+                                                </Select.Option>
+                                            ))}
                                         </Select>
                                     </Form.Item>
-                                    <Form.Item noStyle name={[subField.name, 'buyPrice']}>
-                                        <Input
-                                            placeholder="매입단가"
-                                            onBlur={() => {
-                                                form.setFieldsValue({
-                                                    [`list[${index}].price`] : calculateAmount(
-                                                        form.getFieldValue().list[`${index}`].buyPrice,
-                                                        form.getFieldValue().list[`${index}`].requiredAmount
-                                                    ),
-                                                });
-                                                console.log("금액:", form.getFieldValue(`list[${index}].price`));
-                                            }}
-                                        />
+                                    <Form.Item
+                                        {...restField}
+                                        noStyle
+                                        name={[name, 'buyPrice']}
+                                    >
+                                        <Input placeholder="매입단가" disabled />
                                     </Form.Item>
-                                    <Form.Item noStyle name={[subField.name, 'requiredAmount']}>
-                                        <Input
-                                            placeholder="소요량"
-                                            onBlur={() => {
-                                                form.setFieldsValue({
-                                                    [`list[${index}].price`] : calculateAmount(
-                                                        form.getFieldValue().list[`${index}`].buyPrice,
-                                                        form.getFieldValue().list[`${index}`].requiredAmount
-                                                    ),
-                                                });
-                                                console.log("금액:", form.getFieldValue(`list[${index}].price`));
-                                            }}
-                                        />
+                                    <Form.Item
+                                        {...restField}
+                                        noStyle
+                                        name={[name, 'requiredAmount']}
+                                        rules={[{ required: true, message: '소요량을 입력해주세요.' }]}
+                                    >
+                                        <Input placeholder="소요량" onChange={(value) => onPriceChange(value, form, name)}/>
                                     </Form.Item>
-                                    <Form.Item noStyle name={[subField.name, 'price']}>
-                                        <Input placeholder="금액" disabled={true}/>
+                                    <Form.Item
+                                        {...restField}
+                                        noStyle
+                                        name={[name, 'price']}
+                                    >
+                                        <Input placeholder="금액" disabled/>
                                     </Form.Item>
-                                    <Form.Item noStyle name={[subField.name, 'note']}>
+                                    <Form.Item
+                                        {...restField}
+                                        noStyle
+                                        name={[name, 'note']}
+                                    >
                                         <Input placeholder="비고" />
                                     </Form.Item>
-                                    <CloseOutlined
-                                        onClick={() => {
-                                            subOpt.remove(subField.name);
-                                        }}
-                                    />
+                                    <MinusCircleOutlined onClick={() => remove(name)} />
                                 </Space>
                             ))}
-                            <Button type="dashed" onClick={() => subOpt.add()} block>
-                                + 자재 추가
+                            <Button
+                                type="dashed"
+                                onClick={() => add()}
+                                icon={<PlusOutlined />}
+                            >
+                                자재추가
                             </Button>
                         </div>
                     )}
                 </Form.List>
             </Form.Item>
-
-            {/*<Form.Item noStyle shouldUpdate>*/}
-            {/*    {() => (*/}
-            {/*        <Typography>*/}
-            {/*            <pre>{JSON.stringify(form.getFieldsValue(), null, 2)}</pre>*/}
-            {/*        </Typography>*/}
-            {/*    )}*/}
-            {/*</Form.Item>*/}
         </>
     );
 };
